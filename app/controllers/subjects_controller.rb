aca49b3e134657c9ebc577282cc4ac7bbe9c6f43
Kyle Helsing <kylehelsing@gmail.com>
2017-02-21 15:36:56 -0500

    finished CRUD

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,6 +1,6 @@
 class SubjectsController < ApplicationController
 
-  layout false
+  layout "admin"
 
   def index
     @subjects = Subject.sorted
@@ -11,11 +11,54 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new({:name => "Default"})
+  end
+
+  def create
+    #Instantiate a new object using parameters
+    @subject = Subject.new(subject_params)
+    #save the object
+    if @subject.save
+      #redirect to index action
+      flash[:notice] = "Subject created successfully"
+      redirect_to(:action => 'index')
+    else
+      #if fails, redisplay form so user can fix problems
+      render('new')
+    end
   end
 
   def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    #Find an existing object using parameters
+    @subject = Subject.find(params[:id])
+    #Update the object
+    if @subject.update_attributes(subject_params)
+    #If update succeeds, redirect to index action
+      flash[:notice] = "Subject updated successfully"
+      redirect_to(:action => 'show', :id => @subject.id)
+    else
+      #if update fails, redisplay form so user can fix problems
+      render('edit')
+    end
   end
 
   def delete
+    @subject = Subject.find(params[:id])
   end
+
+  def destroy
+    subject = Subject.find(params[:id]).destroy
+    flash[:notice] = "Subject '#{subject.name}' destroyed successfully"
+    redirect_to(:action => 'index')
+  end
+
+  private
+    def subject_params
+      params.require(:subject).permit(:name, :position, :visible)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ SimpleCms::Application.routes.draw do
   root "demo#index"
 
   #get "demo/index"
-  match ':controller(/:action(/:id))', :via => :get
+  match ':controller(/:action(/:id))', :via => [:get, :post]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
